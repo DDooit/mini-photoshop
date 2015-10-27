609a7c4fb70962492c9cc88d2628a95232455d34
DDooit <doohllo7@naver.com>
2015-10-27 17:05:40 +0900

    mini-photoshop start

---
+++ pixastic.worker.control.js
@@ -0,0 +1,19 @@
+﻿if('function' === typeof importScripts) {
+	importScripts("pixastic.js");
+	importScripts("pixastic.effects.js");
+
+		//document.write('<scr'+'ipt type="text/javascript" src="pixastic.js" ></scr'+'ipt>');
+		//document.write('<scr'+'ipt type="text/javascript" src="pixastic.effects.js" ></scr'+'ipt>');
+
+	var worker = new Pixastic.Worker();
+
+	worker.onmessage = function(message) {
+	    postMessage(message.data);
+	}
+
+	onmessage = function(message) {
+	    worker.postMessage(message.data);
+	}
+}
+
+
